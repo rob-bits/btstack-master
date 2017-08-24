1a6e92f1907062a1cf7b5d562ad111e3e91105c8
Matthias Ringwald <matthias@ringwald.ch>
2017-08-24 12:03:34 +0200

    mesh: btstack boots up against simulator

--- test/mesh/simulator.py
+++ test/mesh/simulator.py
@@ -15,11 +15,17 @@ import select
 import subprocess
 import sys
 
+from Crypto.Cipher import AES
+from Crypto import Random
+
+def little_endian_read_16(buffer, pos):
+    return ord(buffer[pos]) + (ord(buffer[pos+1]) << 8)
+
 class H4Parser:
     state = "H4_W4_PACKET_TYPE"
     packet_type = "NONE"
     bytes_to_read = 1
-    buffer = []
+    buffer = ''
 
     def __init__(self):
         self.reset()
@@ -29,27 +35,26 @@ class H4Parser:
 
     def reset(self):
         self.bytes_to_read = 1
-        self.buffer = []
+        self.buffer = ''
         self.state = "H4_W4_PACKET_TYPE"
 
     def parse(self, data):
-        self.buffer.append(data)
+        self.buffer += data
         self.bytes_to_read -= 1
-        print(self.buffer, self.bytes_to_read, self.state)
+        # print(self.buffer, self.bytes_to_read, self.state)
         if self.bytes_to_read == 0:
             if self.state == "H4_W4_PACKET_TYPE":
+                self.buffer = ''
                 if data == chr(1):
                     # cmd
                     self.packet_type = "CMD"
                     self.state = "W4_CMD_HEADER"
                     self.bytes_to_read = 3
-                    self.buffer = []
                 if data == chr(2):
                     # acl 
                     self.packet_type = "ACL"
                     self.state = "W4_ACL_HEADER"
                     self.bytes_to_read = 4
-                    self.buffer = []
                 return
             if self.state == "W4_CMD_HEADER":
                 self.bytes_to_read = ord(self.buffer[2])
@@ -58,7 +63,7 @@ class H4Parser:
                     return
                 # fall through
             if self.state == "W4_ACL_HEADER":
-                self.bytes_to_read = ord(self.buffer[2]) + ord(self.buffer[3]) << 8
+                self.bytes_to_read = little_endian_read_16(buffer, 2)
                 self.state = "H4_W4_PAYLOAD"
                 if self.bytes_to_read > 0:
                     return
@@ -70,8 +75,12 @@ class H4Parser:
 
 class HCIController:
 
+    # fields
     parser = H4Parser()
     fd = -1
+    random = Random.new()
+    name = 'BTstack Mesh Simulator'
+    bd_addr = '\x11\x22\x33\x44\x55\x66'
 
     def __init__(self):
         print('HCI Controller()')
@@ -80,23 +89,95 @@ class HCIController:
     def parse(self, data):
         self.parser.parse(data)
 
-    def packet_handler(self, packet_type, packet):
-        print (packet_type, packet)
-        print ('Received HCI Reset, sending Command Complete')
-        # TODO: check command, send appropriate response
-        os.write(self.fd, "\x04\x0e\x04\x01\x03\x0c\x00")
-
     def set_fd(self,fd):
         self.fd = fd
 
-# parser = H4Parser()
-# parser.set_packet_handler(packet_handler)
+    def set_bd_addr(self, bd_addr):
+        self.bd_addr = bd_addr
+
+    def set_name(self, bd_addr):
+        self.name = name
+
+    def emit_command_complete(self, opcode, result):
+        # type, event, len, num commands, opcode, result
+        os.write(self.fd, '\x04\x0e' + chr(3 + len(result)) + chr(1) + chr(opcode & 255)  + chr(opcode >> 8) + result)
+
+    def packet_handler(self, packet_type, packet):
+        # print (packet_type, packet)
+        opcode = little_endian_read_16(packet, 0)
+        if opcode == 0x0c03:
+            self.emit_command_complete(opcode, '\x00')
+            return
+        if opcode == 0x1001:
+            self.emit_command_complete(opcode, '\x00\x10\x00\x06\x86\x1d\x06\x0a\x00\x86\x1d')
+            return
+        if opcode == 0x0c14:
+            self.emit_command_complete(opcode, '\x00' + self.name)
+            return
+        if opcode == 0x1002:
+            self.emit_command_complete(opcode, '\x00\xff\xff\xff\x03\xfe\xff\xff\xff\xff\xff\xff\xff\xf3\x0f\xe8\xfe\x3f\xf7\x83\xff\x1c\x00\x00\x00\x61\xf7\xff\xff\x7f\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00')
+            return
+        if opcode == 0x1009:
+            # read bd_addr
+            self.emit_command_complete(opcode, '\x00' + self.bd_addr[::-1])            
+            return
+        if opcode == 0x1005:
+            # read buffer size
+            self.emit_command_complete(opcode, '\x00\x36\x01\x40\x0a\x00\x08\x00')
+            return
+        if opcode == 0x1003:
+            # read local supported features
+            self.emit_command_complete(opcode, '\x00\xff\xff\x8f\xfe\xf8\xff\x5b\x87')
+            return
+        if opcode == 0x0c01:
+            self.emit_command_complete(opcode, '\x00')
+            return
+        if opcode == 0x2002:
+            # le read buffer size
+            self.emit_command_complete(opcode, '\x00\x00\x00\x00')
+            return
+        if opcode == 0x200f:
+            # read whitelist size
+            self.emit_command_complete(opcode, '\x00\x19')
+            return
+        if opcode == 0x200b:
+            # set scan parameters
+            self.emit_command_complete(opcode, '\x00')
+            return
+        if opcode == 0x0c6d:
+            # write le host supported
+            self.emit_command_complete(opcode, '\x00')
+            return
+        if opcode == 0x2017:
+            # LE Encrypt - key 16, data 16
+            key = packet[18:2:-1]
+            data = packet[35:18:-1]
+            cipher = AES.new(key)
+            result = cipher.encrypt(data)
+            self.emit_command_complete(opcode, result[::-1])
+            return
+        if opcode == 0x2018:
+            # LE Rand
+            self.emit_command_complete(opcode, '\x00' + self.random.read(8))
+            return
+        if opcode == 0x2006:
+            # Set Adv Params
+            self.emit_command_complete(opcode, '\x00')
+            return
+        if opcode == 0x2008:
+            # Set Adv Data
+            self.emit_command_complete(opcode, '\x00')
+            return
+        if opcode == 0x200a:
+            # Set Adv Enable
+            self.emit_command_complete(opcode, '\x00')
+            return
+        print("Opcode 0x%0x not handled!" % opcode)
 
 # parse configuration file passed in via cmd line args
 # TODO
 
 # create ptys
-# TODO as requested
 (master, slave) = pty.openpty()
 slave_ttyname = os.ttyname(slave)
 print('slave  %u %s' % (slave, slave_ttyname))
@@ -110,7 +191,7 @@ subprocess.Popen(['./le_counter', '-u', slave_ttyname])
 # get response from more than one
 while True:
     (read_ready, write_ready, exception_ready) = select.select([master],[],[])
-    print(read_ready, write_ready, exception_ready)    
+    # print(read_ready, write_ready, exception_ready)    
     c = os.read(master, 1)
     controller.parse(c)
 
