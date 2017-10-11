b94d75da5d94fce154909e4b08c7b93dbad5341a
Matthias Ringwald <matthias@ringwald.ch>
2017-10-11 12:16:35 +0200

    mesh: calculate confirmation value using micro-ecc

--- test/mesh/mesh.c
+++ test/mesh/mesh.c
@@ -46,6 +46,8 @@
 #include "ble/mesh/beacon.h"
 #include "classic/rfcomm.h" // for crc8
 #include "btstack.h"
+#include "uECC.h"
+#include "crypto.h"
 
 static btstack_packet_callback_registration_t hci_event_callback_registration;
 
@@ -64,9 +66,12 @@ const uint8_t adv_data_len = sizeof(adv_data);
 const static uint8_t device_uuid[] = { 0x00, 0x1B, 0xDC, 0x08, 0x10, 0x21, 0x0B, 0x0E, 0x0A, 0x0C, 0x00, 0x0B, 0x0E, 0x0A, 0x0C, 0x00 };
 
 // micro-ecc
-
 static uint8_t ec_d[32];
 static uint8_t ec_q[64];
+
+// remote ecc
+static uint8_t remote_ec_q[64];
+static uint8_t dhkey[32];
 
 static void parse_hex(uint8_t * buffer, const char * hex_string){
     while (*hex_string){
@@ -83,6 +88,19 @@ static void use_fixed_ec_keypair(void){
     parse_hex(ec_d, ec_d_string);
     parse_hex(&ec_q[0],  ec_qx_string);
     parse_hex(&ec_q[32], ec_qy_string);
+}
+
+
+static void provisioning_calculate_dhkey(void){
+#if uECC_SUPPORTS_secp256r1
+    // standard version
+    uECC_shared_secret(remote_ec_q, ec_d, dhkey, uECC_secp256r1());
+#else
+    // static version
+    uECC_shared_secret(remote_ec_q, ec_d, dhkey);
+#endif
+    printf("DHKEY: ");
+    printf_hexdump(dhkey, sizeof(dhkey));
 }
 
 //
@@ -201,13 +219,19 @@ static prov_state_t prov_state;
 #endif
 
 static uint8_t  prov_buffer_out[100];   // TODO: how large are prov messages?
+// ConfirmationInputs = ProvisioningInvitePDUValue || ProvisioningCapabilitiesPDUValue || ProvisioningStartPDUValue || PublicKeyProvisioner || PublicKeyDevice
+static uint8_t  prov_confirmation_inputs[1 + 11 + 5 + 64 + 64];
+static uint8_t  prov_authentication_action;
 
 static void provisioning_handle_invite(uint8_t *packet, uint16_t size){
 
-    UNUSED(size);
-    UNUSED(packet);
+    if (size != 1) return;
+
+    // store for confirmation inputs: len 1
+    memcpy(&prov_confirmation_inputs[0], packet, 1);
 
     // handle invite message
+
     // TODO: store Attention Timer State
 
     // setup response 
@@ -230,7 +254,7 @@ static void provisioning_handle_invite(uint8_t *packet, uint16_t size){
     prov_buffer_out[6] = 8; 
 
     /* Output OOB Action */
-    big_endian_store_16(prov_buffer_out, 7, MESH_OUTPUT_OOB_NUMBER | MESH_OUTPUT_OOB_STRING);
+    big_endian_store_16(prov_buffer_out, 7, MESH_OUTPUT_OOB_NUMBER); //  | MESH_OUTPUT_OOB_STRING);
 
     /* Input OOB Size - max of 8*/
     prov_buffer_out[9] = 8; 
@@ -238,18 +262,44 @@ static void provisioning_handle_invite(uint8_t *packet, uint16_t size){
     /* Input OOB Action */
     big_endian_store_16(prov_buffer_out, 10, MESH_INPUT_OOB_STRING | MESH_OUTPUT_OOB_NUMBER);
 
+    // append invite
+    memcpy(&prov_confirmation_inputs[1], &prov_buffer_out[1], 11);
+
     // send
     pb_adv_send_pdu(prov_buffer_out, 12);
 }
 
+static void provisioning_handle_start(uint8_t * packet, uint16_t size){
+
+    if (size != 5) return;
+
+    // store for confirmation inputs: len 5
+    memcpy(&prov_confirmation_inputs[12], packet, 5);
+
+    // output authentication action
+    prov_authentication_action = packet[3];
+    printf("Authentication Action: %02x\n", prov_authentication_action);
+}
+
 static void provisioning_handle_public_key(uint8_t *packet, uint16_t size){
 
-    UNUSED(size);
-    UNUSED(packet);
+    if (size != sizeof(remote_ec_q)) return;
+
+    // store for confirmation inputs: len 64
+    memcpy(&prov_confirmation_inputs[17], packet, 64);
+
+    // store remote q
+    memcpy(remote_ec_q, packet, sizeof(remote_ec_q));
+
+    // calculate DHKey
+    provisioning_calculate_dhkey();
 
     // setup response 
     prov_buffer_out[0] = MESH_PROV_PUB_KEY;
     memcpy(&prov_buffer_out[1], ec_q, 64);
+
+    // append public key
+    memcpy(&prov_confirmation_inputs[81], &prov_buffer_out[1], 64);
 
     // send
     pb_adv_send_pdu(prov_buffer_out, 65);
@@ -262,7 +312,34 @@ static void provisioning_handle_confirmation(uint8_t *packet, uint16_t size){
 
     // setup response 
     prov_buffer_out[0] = MESH_PROV_CONFIRM;
-    memset(&prov_buffer_out[1], 0, 16);
+
+    // CalculationInputs
+    uint8_t confirmation_salt[16];
+    printf("CalculationInputs: ");
+    printf_hexdump(prov_confirmation_inputs, sizeof(prov_confirmation_inputs));
+    // ClaculationSalt
+    s1(confirmation_salt, prov_confirmation_inputs, sizeof(prov_confirmation_inputs));
+    printf("CalculationSalt: ");
+    printf_hexdump(confirmation_salt, sizeof(confirmation_salt));
+    // ConfirmationKey
+    uint8_t confirmation_key[16];
+    k1(confirmation_key, dhkey, sizeof(dhkey), confirmation_salt, (uint8_t*) "prck", 4);
+    printf("ConfirmationKey: ");
+    printf_hexdump(confirmation_key, sizeof(confirmation_key));
+    // ConfirmationDevice
+    uint8_t confirmation_device[16];
+
+    uint8_t auth_value[16];
+    memset(auth_value, 0, sizeof(auth_value));
+    auth_value[15] = prov_authentication_action;
+
+    // TODO: use random
+    memset(&prov_confirmation_inputs[ 0], 0, 16);
+    memcpy(&prov_confirmation_inputs[16], auth_value, 16);
+    aes_cmac(confirmation_device, confirmation_key, prov_confirmation_inputs, 32);
+    printf("ConfirmationDevice: ");
+    printf_hexdump(confirmation_key, sizeof(confirmation_key));
+    memcpy(&prov_buffer_out[1], confirmation_device, 16);
 
     // send
     pb_adv_send_pdu(prov_buffer_out, 17);
@@ -294,30 +371,31 @@ static void provisioning_handle_pdu(uint8_t packet_type, uint16_t channel, uint8
                 case MESH_PROV_INVITE:
                     printf("MESH_PROV_INVITE\n");
                     printf_hexdump(packet, size);
-                    provisioning_handle_invite(packet, size);
+                    provisioning_handle_invite(&packet[1], size-1);
                     break;
                 case MESH_PROV_START:
                     printf("MESH_PROV_START\n");
-                    printf_hexdump(packet, size);
+                    printf_hexdump(&packet[1], size-1);
+                    provisioning_handle_start(&packet[1], size-1);
                     break;
                 case MESH_PROV_PUB_KEY:
                     printf("MESH_PROV_PUB_KEY\n");
-                    printf_hexdump(packet, size);
-                    provisioning_handle_public_key(packet, size);
+                    printf_hexdump(&packet[1], size-1);
+                    provisioning_handle_public_key(&packet[1], size-1);
                     break;
                 case MESH_PROV_CONFIRM:
                     printf("MESH_PROV_CONFIRM\n");
-                    printf_hexdump(packet, size);
-                    provisioning_handle_confirmation(packet, size);
+                    printf_hexdump(&packet[1], size-1);
+                    provisioning_handle_confirmation(&packet[1], size-1);
                     break;
                 case MESH_PROV_RANDOM:
                     printf("MESH_PROV_RANDOM\n");
-                    printf_hexdump(packet, size);
-                    provisioning_handle_random(packet, size);
+                    printf_hexdump(&packet[1], size-1);
+                    provisioning_handle_random(&packet[1], size-1);
                     break;
                 default:
                     printf("TODO: handle provisioning msg type %x\n", packet[0]);
-                    printf_hexdump(packet, size);
+                    printf_hexdump(&packet[1], size-1);
                     break;
             }            
             break;
