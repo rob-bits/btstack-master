17840f19052d08035f077fb0d14e193605a4d25d
Matthias Ringwald <matthias@ringwald.ch>
2017-11-27 17:41:11 +0100

    h5: rename btstack_uart_block_t to btstack_uart_t and move to btstack_uart.h

--- src/btstack_uart.h
+++ src/btstack_uart.h
@@ -68,4 +68,82 @@ typedef enum {
     BTSTACK_UART_SLEEP_MASK_RTS_LOW_WAKE_ON_RX_EDGE     = 1 << BTSTACK_UART_SLEEP_RTS_LOW_WAKE_ON_RX_EDGE
 } btstack_uart_sleep_mode_mask_t;
 
+
+typedef struct {
+    /**
+     * init transport
+     * @param uart_config
+     */
+    int (*init)(const btstack_uart_config_t * uart_config);
+
+    /**
+     * open transport connection
+     */
+    int (*open)(void);
+
+    /**
+     * close transport connection
+     */
+    int (*close)(void);
+
+    /**
+     * set callback for block received. NULL disables callback
+     */
+    void (*set_block_received)(void (*block_handler)(void));
+
+    /**
+     * set callback for sent. NULL disables callback
+     */
+    void (*set_block_sent)(void (*block_handler)(void));
+
+    /**
+     * set baudrate
+     */
+    int (*set_baudrate)(uint32_t baudrate);
+
+    /**
+     * set parity
+     */
+    int  (*set_parity)(int parity);
+
+    /**
+     * set flowcontrol
+     */
+    int  (*set_flowcontrol)(int flowcontrol);
+
+    /**
+     * receive block
+     */
+    void (*receive_block)(uint8_t *buffer, uint16_t len);
+
+    /**
+     * send block
+     */
+    void (*send_block)(const uint8_t *buffer, uint16_t length);
+
+    // support for sleep modes in TI's H4 eHCILL and H5
+
+    /**
+     * query supported wakeup mechanisms
+     * @return supported_sleep_modes mask
+     */
+     int (*get_supported_sleep_modes)(void);
+
+    /**
+     * set UART sleep mode - allows to turn off UART and it's clocks to save energy
+     * Supported sleep modes:
+     * - off: UART active, RTS low if receive_block was called and block not read yet
+     * - RTS high, wake on CTS: RTS should be high. On CTS pulse, UART gets enabled again and RTS goes to low
+     * - RTS low, wake on RX: data on RX will trigger UART enable, bytes might get lost
+     */
+    void (*set_sleep)(btstack_uart_sleep_mode_t sleep_mode);
+
+    /** 
+     * set wakeup handler - needed to notify hci transport of wakeup requests by Bluetooth controller
+     * Called upon CTS pulse or RX data. See sleep modes.
+     */
+    void (*set_wakeup_handler)(void (*wakeup_handler)(void));
+
+} btstack_uart_t;
+
 #endif
