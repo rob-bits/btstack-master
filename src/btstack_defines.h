wrtcoder/btstack

mesh: add PROVISIONING_DATA_PACKET packet type

src/btstack_defines.h
@@ -110,7 +110,10 @@ typedef uint8_t sm_key_t[16];
 
 // PBAP data
 #define PBAP_DATA_PACKET        0x0e
- 
+
+// Mesh Provisioning PDU
+#define PROVISIONING_DATA_PACKET 0x0f
+
 // debug log messages
 #define LOG_MESSAGE_PACKET      0xfc
 

rob-bits/btstack-master

src/btstack_defines.h
@@ -80,7 +80,6 @@ typedef uint8_t sm_key_t[16];
 // hci con handles (12 bit): 0x0000..0x0fff
 #define HCI_CON_HANDLE_INVALID 0xffff
 
-
 #define DAEMON_EVENT_PACKET     0x05
     
 // L2CAP data
@@ -114,7 +113,9 @@ typedef uint8_t sm_key_t[16];
 // AVRCP browsing data
 #define AVRCP_BROWSING_DATA_PACKET     0x0f
 
- 
+// Mesh Provisioning PDU
+#define PROVISIONING_DATA_PACKET 0x10
+
 // debug log messages
 #define LOG_MESSAGE_PACKET      0xfc
 