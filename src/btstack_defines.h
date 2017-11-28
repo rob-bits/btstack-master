henriclinden/btstack

mesh: add MESH meta event

src/btstack_defines.h
@@ -975,6 +975,7 @@ typedef uint8_t sm_key_t[16];
 #define HCI_EVENT_PBAP_META                                0xEE
 #define HCI_EVENT_HID_META                                 0xEF
 #define HCI_EVENT_A2DP_META                                0xF0
+#define HCI_EVENT_MESH_META                                0xF1
 
 // Potential other meta groups
 // #define HCI_EVENT_BNEP_META                                0xxx
@@ -1791,7 +1792,13 @@ typedef uint8_t sm_key_t[16];
  * @format 12
  * @param subevent_code
  * @param hid_cid
-*/
+ */
 #define HID_SUBEVENT_CAN_SEND_NOW                                          0x03
+
+/**
+ * @format 1
+ * @param subevent_code
+ */
+#define MESH_SUBEVENT_CAN_SEND_NOW                                         0x01
 
 #endif

rob-bits/btstack-master

src/btstack_defines.h
@@ -987,6 +987,7 @@ typedef uint8_t sm_key_t[16];
 #define HCI_EVENT_HID_META                                 0xEF
 #define HCI_EVENT_A2DP_META                                0xF0
 #define HCI_EVENT_HIDS_META                                0xF1
+#define HCI_EVENT_MESH_META                                0xF2
 
 // Potential other meta groups
 // #define HCI_EVENT_BNEP_META                                0xxx
@@ -1892,7 +1893,7 @@ typedef uint8_t sm_key_t[16];
  * @format 12
  * @param subevent_code
  * @param hid_cid
-*/
+ */
 #define HID_SUBEVENT_CAN_SEND_NOW                                          0x03
 
 // HIDS Meta Event Group
@@ -1935,5 +1936,11 @@ typedef uint8_t sm_key_t[16];
  * @param enable
 */
 #define HIDS_SUBEVENT_INPUT_REPORT_ENABLE                                   0x05
+
+/**
+ * @format 1
+ * @param subevent_code
+ */
+#define MESH_SUBEVENT_CAN_SEND_NOW                                          0x01
 
 #endif